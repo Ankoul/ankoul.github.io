cb6e2dee7f1fe4229e5c29f29779340c2b8f5e36
Gilson <gilsonn_souza@hotmail.com>
2017-06-21 04:02:35 -0300

    chilli authenticaiton

---
+++ czChilliAuth.js
@@ -0,0 +1,29 @@
+(function(){
+	chilliController.onUpdate = function(){
+		if(chilliController.clientState == chilliController.stateCodes.AUTH){
+			window.location = normalizeUrl(getURLParam("userurl"));
+		}
+	  }
+	  var doLogon2 = function(response){
+		var metaContent = jQuery(response)[1].content;
+		response = getURLParam("response",metaContent);
+		chilliController.logon2("usertest",response);
+		setTimeout(chilliController.onUpdate, 1000);
+	  };	
+	  var doLogon1 = function(){
+		jQuery.post("login.chi",{username:"usertest",password:"passwd",userurl:"http://www.google.com"},doLogon2,"html");
+	  };
+	  var authStateChanged = function(user) {
+          if (user) {
+            doLogon1();
+          } 
+		  
+	   };
+      var initApp = function() {
+		chilliController.onUpdate();
+        firebase.auth().onAuthStateChanged(authStateChanged, function(error) {
+          console.log(error);
+        });
+      };
+      window.addEventListener('load', initApp);
+})();
